ce4d13672143f072561a3cad0ea24b039cf32cfc
FranciscoCosme22 <fcosmer@miumg.edu.gt>
2022-07-20 23:03:35 -0600

    Mi primer archivo creado

---
+++ HolaMundo/src/com/ejemplo/holamundo.java
@@ -0,0 +1,17 @@
+package com.ejemplo;
+
+import java.util.Scanner;
+
+public class holamundo {
+    public static void main(String[] args) {
+
+        String nombre;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Ingrese Nombre");
+        nombre = in.next();
+        System.out.println("Bienvenido a Progra 2 2022 "  + nombre);
+
+    }
+}
